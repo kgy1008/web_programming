3287c6930c83e7dcc4bb53d8028d39393b01aaed
kgy1008 <gayeon1008@g.skku.edu>
2024-06-09 17:16:48 +0900

    set dark mode

---
+++ javascript/toggle.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const toggleDarkMode = document.getElementById('toggle-dark-mode');
+    
+    if (localStorage.getItem('dark-mode') === 'enabled') {
+        document.body.classList.add('dark-mode');
+        toggleDarkMode.checked = true;
+    }
+
+    toggleDarkMode.addEventListener('change', () => {
+        if (toggleDarkMode.checked) {
+            document.body.classList.add('dark-mode');
+            localStorage.setItem('dark-mode', 'enabled');
+        } else {
+            document.body.classList.remove('dark-mode');
+            localStorage.setItem('dark-mode', 'disabled');
+        }
+    });
+});
